3b9179c8fd467343658670bbb004eadfc2997968
Brian-Bruce LO <brian-bruce.lo@efrei.net>
2024-05-14 15:23:13 +0200

    Added model with trainer config file

--- Assets/Car/Scripts/AgentCarController.cs
+++ Assets/Car/Scripts/AgentCarController.cs
@@ -2,13 +2,19 @@ using UnityEngine;
 using Unity.MLAgents;
 using Unity.MLAgents.Sensors;
 using Unity.MLAgents.Actuators;
+using Unity.MLAgents.Policies;
+
 
 public class CarAgentController : Agent
 {
     private Vector3 initialPosition;
+    private Quaternion initialRotation;
+    private Vector3 initialParkingPlacePosition;
+    private Quaternion initialParkingPlaceRotation;
     private float currentSteerAngle;
     private float currentBreakForce;
     private bool isBreaking;
+    private bool isParked;
 
     [Header("Car Settings")]
     [SerializeField] private float motorForce;
@@ -32,25 +38,42 @@ public class CarAgentController : Agent
     [SerializeField] private Transform border;
 
     [Header("Agent Settings")]
-    [SerializeField] private short numBeaconRequired;
+    [SerializeField] private short numBeaconRequired = 6;
+    [SerializeField] private float parkingThreshold = 0.25f;
 
     public override void Initialize()
     {
         initialPosition = transform.position;
+        initialRotation = transform.rotation;
+        initialParkingPlacePosition = parkingPlace.transform.position;
+        initialParkingPlaceRotation = parkingPlace.transform.rotation;
     }
 
     public override void OnEpisodeBegin()
     {
         transform.position = initialPosition;
+        transform.rotation = initialRotation;
+        parkingPlace.transform.position = initialParkingPlacePosition;
+        parkingPlace.transform.rotation = initialParkingPlaceRotation;
+        isParked = false;
     }
 
     public override void CollectObservations(VectorSensor sensor)
     {
-        sensor.AddObservation(transform.localPosition);
-        sensor.AddObservation(parkingPlace.transform.localPosition);
+        sensor.AddObservation(transform.localPosition.x);
+        sensor.AddObservation(transform.localPosition.z);
+        sensor.AddObservation(transform.localEulerAngles.y);
+
+        sensor.AddObservation(frontLeftWheelCollider.rpm);
+
+        sensor.AddObservation(parkingPlace.transform.localPosition.x);
+        sensor.AddObservation(parkingPlace.transform.localPosition.z);
+        sensor.AddObservation(parkingPlace.transform.localEulerAngles.y);
+
+        sensor.AddObservation(Vector3.Distance(transform.position, parkingPlace.transform.position));
+
         sensor.AddObservation(parkingPlace.GetComponent<ParkingPlace>().numBeaconInPlace);
     }
-
 
     public override void Heuristic(in ActionBuffers actionsOut)
     {
@@ -76,9 +99,16 @@ public class CarAgentController : Agent
         HandleSteering();
         UpdateWheels();
 
-        if (parkingPlace.GetComponent<ParkingPlace>().numBeaconInPlace == numBeaconRequired)
+        if (!isParked && IsParkedCorrectly())
         {
+            isParked = true;
             AddReward(20f);
+            ChangeParkingZoneColor(Color.green);
+            EndEpisode();
+        }
+        else if (parkingPlace.GetComponent<ParkingPlace>().numBeaconInPlace == numBeaconRequired)
+        {
+            AddReward(10f);
             ChangeParkingZoneColor(Color.green);
             EndEpisode();
         }
@@ -86,6 +116,24 @@ public class CarAgentController : Agent
         {
             ChangeParkingZoneColor(Color.red);
         }
+    }
+
+    private bool IsParkedCorrectly()
+    {
+        if (!parkingPlace.GetComponent<Collider>().bounds.Contains(transform.position))
+        {
+            return false;
+        }
+        float dotProduct = Vector3.Dot(transform.forward, parkingPlace.transform.forward);
+        if (Mathf.Abs(dotProduct) < 0.9f)
+        {
+            return false;
+        }
+        if (Vector3.Distance(transform.position, parkingPlace.transform.position) > parkingThreshold)
+        {
+            return false;
+        }
+        return true;
     }
 
     private void ChangeParkingZoneColor(Color newColor)
