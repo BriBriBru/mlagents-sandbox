167beccaf0898ac30eab7b7a328d333a487fb93a
Brian-Bruce LO <brian-bruce.lo@efrei.net>
2024-05-31 15:55:18 +0200

    Tested game manually

--- Assets/Tag/Scripts/CatController.cs
+++ Assets/Tag/Scripts/CatController.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 
 public class CatController : MonoBehaviour
 {
+    [Header("Speed variables")]
     [SerializeField] private float moveSpeed = 10f;
     [SerializeField] private float rotateSpeed = 100f;
 
@@ -25,6 +26,14 @@ public class CatController : MonoBehaviour
         if (Input.GetKey(KeyCode.RightArrow))
         {
             transform.Rotate(Vector3.up * rotateSpeed * Time.deltaTime);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.CompareTag("Mouse"))
+        {
+            Debug.Log("TOUCHE");
         }
     }
 }
